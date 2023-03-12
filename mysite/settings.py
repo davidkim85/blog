8cc7a3e4faaf4b54fa72b53c13536e1655fa57e9
David Furman <poramok@gmail.com>
2023-03-13 00:54:44 +0200

    adde

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-
+MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
